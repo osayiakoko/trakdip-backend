e271d1d8c04cb49be00bfa5c3147afc2be8daa7e
Osayi Akoko <osayistreams@gmail.com>
2024-03-28 14:50:45 +0100

    use hyphen

--- src/api/core/utils.py
+++ src/api/core/utils.py
@@ -3,5 +3,5 @@ from datetime import datetime, timezone
 
 def generate_unique_filename(filename):
     ext = filename.split(".")[-1]
-    name = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S_%f")
+    name = datetime.now(tz=timezone.utc).strftime("%Y%m%d-%H%M%S-%f")
     return f"{name}.{ext}"
