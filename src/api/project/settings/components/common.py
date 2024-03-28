cf9699bed174f0a7a7fbee7830198d6dd11a8ba3
Osayi Akoko <osayistreams@gmail.com>
2024-03-28 21:23:16 +0100

    add dependencies

--- src/api/project/settings/components/common.py
+++ src/api/project/settings/components/common.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_spectacular",
+    "django_filters",
     # Apps
     "core.apps.CoreConfig",
     "doc.apps.DocConfig",
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
